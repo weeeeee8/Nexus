6079fc90141dd4a39996a745d7a7efc4cb601129
RVCDv <101155145+weeeeee8@users.noreply.github.com>
2022-12-23 20:33:32 +0800

    w

--- src/env/main.lua
+++ src/env/main.lua
@@ -2,7 +2,7 @@ return {
     Default = function()
         local HydraLibrary = import('env/lib/Hydra')
         
-        local __version__ = import('ver.txt')
+        local __version__ = import('env/ver.txt')
         extern("NEXUS_VERSION", __version__, true)
 
         local Window = HydraLibrary.new("Nexus", string.format("Hello %s,\nthanks for using Nexus!", game.Players.LocalPlayer.Name), NEXUS_VERSION)
