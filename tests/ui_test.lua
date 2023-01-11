0cff257d2c51d78929ffe26ed31a1a17af228df0
RVCDv <101155145+weeeeee8@users.noreply.github.com>
2023-01-11 20:19:45 +0800

    Update ui_test.lua

--- tests/ui_test.lua
+++ tests/ui_test.lua
@@ -2,7 +2,7 @@ local user = "weeeeeee8"
 local branch = "main"
 local url = string.format("https://raw.githubusercontent.com/%s/AquaUI/%s/source.lua", user, branch)
 
-loadstring(game:HttpGet(url), 'Aqua UI Test')()
+loadstring(game:HttpGet(url))()
 
 local UILibrary = Aqua.UI
 
