a94a7e3cfeb6fb78398076a4edcecd067e7da3bf
RVCDv <101155145+weeeeee8@users.noreply.github.com>
2023-01-11 21:03:48 +0800

    Update import.lua

--- src/modules/import.lua
+++ src/modules/import.lua
@@ -12,17 +12,9 @@ return function(assetOrUrl: string)
     local foundPackage
     if assetOrUrl:find("rbxassetid://", 1) then
         foundPackage = game:GetObjects(assetOrUrl)[1]
-    elseif assetOrUrl:find("https://", 1) then
+    else
         local _tbl = string.split(assetOrUrl, "/")
         foundPackage = loadstring(game:HttpGet(url .. assetOrUrl), _tbl[# _tbl])()
-    else
-        local errMsg = string.format('"%s" is faulty or unsupported URL.', assetOrUrl)
-        if Aqua then
-            Aqua.Console.Error(errMsg)
-            Aqua.Console.Terminate()
-        else
-            error(errMsg)
-        end
     end
 
     importCache[assetOrUrl] = foundPackage
